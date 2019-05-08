bd148198a1fe61581760b81f461d6b6d25755d65
ggq <ggq18663278150@sian.com>
2019-05-08 09:13:37 +0800

    添加了启动脚本，并在每次运行前拉取新的代理

--- LiePin/entrypoint.py
+++ LiePin/entrypoint.py
@@ -1,3 +1,8 @@
 from scrapy import cmdline
+from .UpdateProxy import ProxyClass
 
+a = ProxyClass()
+a.verify_proxies()
+a.verify_proxies()
+a.save2file()
 cmdline.execute(['scrapy', 'crawl', 'lp'])
