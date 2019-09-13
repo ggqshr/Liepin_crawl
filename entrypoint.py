3cda0e5d25a86890666aaf79f559ae50fc285c43
ggqshr <942490944@qq.com>
2019-09-13 14:58:35 +0800

    更新代理url

--- entrypoint.py
+++ entrypoint.py
@@ -5,7 +5,7 @@ from LiePin import data5u
 import time
 
 # 获取IP的API接口
-apiUrl = "http://api.xdaili.cn/xdaili-api//greatRecharge/getGreatIp?spiderId=d460f14ed5ae426e8a7164005c61b9e7&orderno=YZ20195179329mb51zm&returnType=1&count=5"
+apiUrl = "http://api.xdaili.cn/xdaili-api//greatRecharge/getGreatIp?spiderId=2eeedc14918546f087abcddafd5ee37d&orderno=YZ20195179329mb51zm&returnType=1&count=5"
 # 获取IP时间间隔，建议为5秒
 fetchSecond = 300
 # 开始自动获取IP
